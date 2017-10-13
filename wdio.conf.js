95e02c4fd3875d473ee07e20f518e3c21e871ccb
Izaac Zavaleta <izaac.zavaleta@moduscreate.com>
2017-10-12 17:25:32 -0700

    commenting a timeout that i used for debugging

--- wdio.conf.js
+++ wdio.conf.js
@@ -172,7 +172,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 100000,
+        // timeout: 100000,
     },
     //
     // =====
