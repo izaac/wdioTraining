9c27f7fd5bc3f7c2834c46965952e2e6714c6e98
Izaac Zavaleta <jorge.izaac@gmail.com>
2017-11-04 17:23:12 -0700

    auto linter fixes

--- test/specs/basic/react_spec.js
+++ test/specs/basic/react_spec.js
@@ -1,19 +1,19 @@
 import { flightsSection } from '../../pages/flights.section';
 
 describe('My flights Test', () => {
-    beforeEach(function() {
-      flightsSection.open('');
-    });
+  beforeEach(() => {
+    flightsSection.open('');
+  });
 
-    it('should click flights and do a basic search', function () {
-        flightsSection.clickflightsTab();
-        flightsSection.flightOriginInput.waitForVisible();
-        expect(flightsSection.flightOriginInput.isVisible()).to.eq(true);
-        flightsSection.clickFlightTypeOneWay();
-        flightsSection.fillFlightOriginInput();
-        flightsSection.fillFlightDestinationInput();
-        flightsSection.fillFlightDepartingDate();
-        flightsSection.clickDatePickerCloseButton();
-        expect(flightsSection.clickSearchButton().resultsList.isVisible()).to.be.true;
-    })
+  it('should click flights and do a basic search', () => {
+    flightsSection.clickflightsTab();
+    flightsSection.flightOriginInput.waitForVisible();
+    expect(flightsSection.flightOriginInput.isVisible()).to.eq(true);
+    flightsSection.clickFlightTypeOneWay();
+    flightsSection.fillFlightOriginInput();
+    flightsSection.fillFlightDestinationInput();
+    flightsSection.fillFlightDepartingDate();
+    flightsSection.clickDatePickerCloseButton();
+    expect(flightsSection.clickSearchButton().resultsList.isVisible()).to.be.true;
+  });
 });
