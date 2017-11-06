45232b7297ae4b225111bfed06d6c37bcd9722de
Izaac Zavaleta <jorge.izaac@gmail.com>
2017-11-06 16:12:35 -0700

    adding back the baseUrl

--- wdio.conf.headless.js
+++ wdio.conf.headless.js
@@ -94,7 +94,7 @@ exports.config = {
   //
   // Set a base URL in order to shorten url command calls. If your url parameter starts
   // with "/", then the base url gets prepended.
-  baseUrl: 'https://www.wikipedia.org',
+  baseUrl: 'https://www.orbitz.com',
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 20000,
